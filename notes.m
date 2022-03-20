72d9a74a9b1c1d7ba7a2eb5c20f99523aa58c827
João Castro <73347405+joaocasr@users.noreply.github.com>
2022-03-20 23:50:55 +0000

    scripts para matlab

--- notes.m
+++ notes.m
@@ -76,7 +76,7 @@ F(2)=-x(1)+2*x(2)-exp(-x(2));
 end
 %%%%%%%%%%%%%%%%%%%%%
 
-%  ---->  spline cúbica completa <----------
+%---->spline cúbica completa<----------
 % OMITIR SEMPRE 2º E PENULTIMO PONTOS
 %tempo        0 2 3 4 5
 %temperatura  0 20 35 60 110
@@ -92,14 +92,14 @@ temp = spline(xx, [10 yy 0.3333], 4.2)
 % Solucao: temp = 71.9525s
 
 
-%% CALCULAR RAIZES POLINOMIOS %%%%
-%P (x) = x5 + 3x3 + 2x + 4 = 0
+%%CALCULAR_RAIZES_DE_POLINOMIOS%%%%
+%P(x)=x5+3x3+2x+4=0
 
 %Resolucao
 c = [1 0 3 0 2 4];
 r = roots(c)
 
-%% Calcular zero de uma funcao num intervalo .>SOLUCAO TEM QUE EXISTIR
+%%Calcular zero de uma funcao num intervalo-->SOLUCAO TEM QUE EXISTIR
 [x,fval,exitflag,output] = fzero('2.*cos(x)-2',[2.*pi,3.*pi])
 
 
