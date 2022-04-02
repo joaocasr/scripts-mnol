a39a842372652b9550449a086bb0203f9131fffe
João Castro <73347405+joaocasr@users.noreply.github.com>
2022-04-03 00:24:30 +0100

    quem é que inventou esta cadeira?

--- exercicios.m
+++ exercicios.m
@@ -35,14 +35,12 @@ end
 % EX15(METODO DE NEWTON)
 
 x0=5; 
-options=optimset('Display','iter');
-[x,fval,exitflag,output]=fsolve(@concentracao,x0,options)
+op=optimset(’tolfun’,5e-2);
+[x,fval,exitflag,output]=fsolve(@concentracao,x0,op)
 
 function f = concentracao(t)
 f = 70.*exp(-1.5.*t) + 25.*exp(-0.075.*t)-9;
 end
-
-
 
 %--------------------------------------------------------------
 
@@ -50,8 +48,8 @@ end
 % EX16(METODO DE NEWTON)
 
 x0=6;
-options=optimset('Display','iter');
-[x,fval,exitflag,output]=fsolve(@ex16,x0,options)
+op=optimset(’tolfun’,1e-3);
+[x,fval,exitflag,output]=fsolve(@ex16,x0,op)
 
 function f = ex16(x)
 f=7*(2-0.9^(x))-10;
@@ -60,34 +58,12 @@ end
 
 %--------------------------------------------------------------
 
-% EX17(METODO DA SECANTE)
+% EX17(METODO DA SECANTE) por Resolução em Matlab/Octave
 
 x1= 40;
 x2= 45;
-fx1 = velocidade(x1);
-fx2 = velocidade(x2);
-x3 = x2-(((x2-x1).*fx2)./(fx2-fx1))
-
-% 1 caso de paragem
-fx3=velocidade(x3);
-cp1x3 = abs(fx3)
-cp2x3 = abs(x3-x2)./abs(x3)
-% abs(fx3)<ε2 && (abs(x3-x2)/abs(x3))<ε1;
-
-% 2 caso de paragem
-
-x4 = x3-((x3-x2).*fx3)./(fx3-fx2)
-%caso de paragem
-fx4=velocidade(x4);
-cp1x4 = abs(fx4)
-cp2x4 = abs(x4-x3)./abs(x4)
-
-x5 = x4-(((x4-x3).*fx4)./(fx4-fx3))
-
-% 3 caso de paragem
-fx5=velocidade(x5);
-cp1x5 = abs(fx5)
-cp2x5 = abs(x5-x4)./abs(x5)
+op=optimset(’tolfun’,5e-2);
+[x,fval,exitflag,output]=fsolve(@velocidade,x1,op)
 
 function f = velocidade(v)
 f = (1 ./ (  (0.08^2) .*  32 ) ) .*  ( 527  .*   (527-470)  .*  log(  1  +  (  (  0.08 .*  v )./ (  527-470  )  )  ) - (527 .* 0.08 .* v)    ) + 300;
@@ -261,7 +237,7 @@ s.coefs
 
 %--------------------------------------------------------------
 
-% EX54(spline cúbica completa)
+% EX54(Minimos quadraddos-polinomio grau2)
 
 x = [0 1.25 2.5 3.75]
 f = [0.260 0.208 0.172 0.145]
@@ -271,5 +247,55 @@ SQR = S2.normr^2
 novo_x = 0:0.01:3.75;
 novo_f = polyval(P2,novo_x);
 plot(x,f,'o',novo_x,novo_f,'r')
+
+
+%--------------------------------------------------------------
+
+% EX55(Minimos quadraddos-polinomio grau1)
+x = [1.2 1.75 1.1 2.0 0.5 0.8 1.0 1.5];
+f = [16 18 16 19 10 11 14 16];
+[P2,S2] = polyfit(x,f,1)
+SQR = S2.normr^2 %residuos
+
+
+%--------------------------------------------------------------
+
+% EX57(Minimos quadraddos-polinomio grau1 e grau 2)
+%  Determinacao do modelo não polinomial 
+
+x = [1.5 2.0 3.0 4.0]
+f = [4.9 3.3 2.0 1.5]
+[P1,S1] = polyfit(x,f,1)%->grau 1
+SQR1 = S1.normr^2
+
+[P2,S2] = polyfit(x,f,2)%->grau 2 
+SQR2 = S2.normr^2
+
+[c,resnorm] = lsqcurvefit(@functionn,[1,1],x,f)
+resnorm
+
+novo_x = 1.5:0.05:4;
+novo_P1 = polyval(P1,novo_x);
+novo_P2 = polyval(P2,novo_x);
+novo_m = functionn(c,novo_x);
+plot(x,f,'o',novo_x,novo_P1,'r',novo_x,novo_P2,'k',novo_x,novo_m,'g')
+
+function f = functionn(c,x)
+f = c(1)./x+c(2).*x;
+end
+
+%--------------------------------------------------------------
+
+% EX59(Minimos quadraddos-polinomio grau1)
+% Determinacao do modelo não polinomial 
+f=[122 188 270 160 120];
+x=[1 3 6 10 12];
+
+
+[c,resnorm] = lsqcurvefit(@functione,[1,1],x,f)
+
+function f = functione(c,x)
+f = c(1).*x + c(2).*sin(x);
+end
 
 
