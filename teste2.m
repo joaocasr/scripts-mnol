8658e073cbb5e392f615ef0017d983aac7ad2f94
João Castro <73347405+joaocasr@users.noreply.github.com>
2022-05-12 09:53:25 +0100

    new update

--- teste2.m
+++ teste2.m
@@ -4,3 +4,18 @@ op = optimset('MaxIter',4);
 function [f] = funcao(x)
     f = max(abs(x(1)),abs(x(2)-1));
 end
+
+%{
+ metodo quasi-Newton e a formula de atualizacao da Hessiana
+ BFGS->predefinido
+
+bfgs - (default) aproxima a Hessiana pela f´ormula BFGS
+lbfgs - aproxima a Hessiana pela Low-memory BFGS (problemas de grandes dimens˜oes)
+dfp - aproxima a Hessiana pela f´ormula DFP
+
+%}
+op=optimset('hessupdate','dfp');
+[x,fval,exitflag,output]=fminunc(@QN,[1,0],op)
+function [ f ] = QN( x )
+f=-(20.*x(1)+26.*x(2)+4.*x(1)*x(2)-4.*x(2));
+end
